4e7c48df72bb21d506a7b3ddd0d64394ecca1deb
anmolpunia21 <48293826+anmolpunia21@users.noreply.github.com>
2020-04-08 17:53:38 +0530

    Add CourseTile and Handle network error

--- lib/Faculty/Course/view/course_list_view.dart
+++ lib/Faculty/Course/view/course_list_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sats/Faculty/Course/model.dart';
 import 'package:sats/Faculty/Course/services.dart';
+import 'package:sats/Faculty/Course/view/course_tile.dart';
 
 class CourseListView extends StatefulWidget {
   @override
@@ -8,16 +9,42 @@ class CourseListView extends StatefulWidget {
 }
 
 class _CourseListViewState extends State<CourseListView> {
+  Widget _body;
+
   _buildListView() {
     return FutureBuilder(
       future: loadcourses(context),
       builder: (context, snapshot) {
-        if (snapshot.hasData) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (snapshot.hasData) {
           List<Course> courses = snapshot.data;
           return ListView.builder(
-              itemCount: courses.length, itemBuilder: (context, index) {
-                return Text(courses[index].name);
+              itemCount: courses.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 8.0,
+                  margin:
+                      new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+                  child: Container(
+                    decoration:
+                        BoxDecoration(color: Color.fromRGBO(72, 129, 219, .9)),
+                    child: CourseTile(courses[index]),
+                  ),
+                );
               });
+        } else if (snapshot.hasError) {
+          return Center(
+            child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    _body = _buildListView();
+                  });
+                },
+                child: Icon(Icons.replay)),
+          );
         } else {
           return Center(
             child: CircularProgressIndicator(),
@@ -28,7 +55,13 @@ class _CourseListViewState extends State<CourseListView> {
   }
 
   @override
+  void initState() {
+    _body = _buildListView();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return _buildListView();
+    return _body;
   }
 }
---
+++ lib/Faculty/Course/view/course_tile.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:sats/Faculty/Course/model.dart';
+
+class CourseTile extends StatelessWidget {
+  final Course course;
+  CourseTile(this.course);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white30,
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    course.name,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Flexible(
+                        child: Text(
+                          course.code,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
